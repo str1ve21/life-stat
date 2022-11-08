508037fb22224588519be0fe942d38519f2345ab
Michail <end3avour21@gmail.com>
2022-11-08 15:51:13 +0700

    better pwa

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { registerSW } from "virtual:pwa-register";
 import App from "./App";
 import "./tailwind.css";
-
-const updateSW = registerSW({
-  onNeedRefresh() {},
-  onOfflineReady() {},
-});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
